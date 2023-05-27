aaaac6dbc66f81eb94252024e36a959920967a74
BielRB <gabrielbarbosa29@hotmail.com>
2023-05-27 18:25:29 -0300

    Adiciona programa dividir e diminuir

--- app.mjs
+++ app.mjs
@@ -3,3 +3,9 @@ console.log(somar(4,8))
 
 const multiplicar=(a,b)=> a*b
 console.log(multiplicar (9,8))
+
+const dividir=(a,b)=> a/b
+console.log(dividir (8,4))
+
+const diminuir=(a,b)=> a-b
+console.log(diminuir (9,3))
